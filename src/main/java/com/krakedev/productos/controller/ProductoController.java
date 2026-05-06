021a297a6a72738411ae251b218d7f54c8aed28e
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-05-06 02:16:27 -0500

    JJBA: Se agrega la clase controller - ProductoController.java - mapeo de metodos y acciones API REST

---
+++ src/main/java/com/krakedev/productos/controller/ProductoController.java
@@ -0,0 +1,51 @@
+package com.krakedev.productos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.krakedev.productos.entidades.Producto;
+import com.krakedev.productos.servicios.ServicioProducto;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+	
+	private ServicioProducto servicioProducto;
+	
+	public ProductoController(ServicioProducto servicioProducto) {
+		this.servicioProducto=servicioProducto;
+	}
+
+	// POST "/productos" - crear producto
+	@PostMapping
+	public Producto crear(@RequestBody Producto producto) {
+		return servicioProducto.crear(producto);
+	}
+
+	// GET "/productos" - listar productos
+	@GetMapping
+	public List<Producto> listar() {
+		return servicioProducto.listar();
+	}
+
+	// GET "/productos/{codigo}" - buscar por codigo
+	@GetMapping("/{codigo}")
+	public Producto buscarPorCodigo(@PathVariable int codigo) {
+		return servicioProducto.buscarPorCodigo(codigo);
+	}
+
+	// PUT "/productos/{codigo}" - actualizar producto
+	@PutMapping("/{codigo}")
+	public Producto actualizar(@PathVariable int codigo, @RequestBody Producto producto) {
+		return servicioProducto.actualizar(codigo, producto);
+	}
+
+	// DELETE "/productos/{codigo}" - eliminar producto
+	@DeleteMapping("/{codigo}")
+	public boolean eliminar(@PathVariable int codigo) {
+		return servicioProducto.eliminar(codigo);
+	}
+}
